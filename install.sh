9b54aea8f6efae57fcfd0352473fcc6c862c066a
yangyinqi <yangyinqi1991@gmail.com>
2017-04-24 23:17:34 +0800

    beta: one key installing scripts

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#search nessesary tools installed.
+exist=0
+git --version > /dev/null && exist=1 || exist=0
+if test "x$exit" = "x0"; then
+	echo "error: git not installed"
+	exit
+fi
+
+exist=0
+vim --version > /dev/null && exist=1 || exist=0
+if test "x$exit" = "x0"; then
+	echo "error: vim not installed"
+	exit
+fi
+
+old_dir=`pwd`
+cd ~
+
+vimrc_exist=
+if test -e ".vimrc"; then
+	vimrc_exist=".vimrc"
+	mv ~/.vimrc ~/.vimrc.bak
+fi
+
+git config --global core.autocrlf false
+
+echo "installing & updating tm_vimcfg..."
+tmp_dir="./tmp_vimrc"
+git clone https://github.com/yangyinqi/tm_vimcfg $tmp_dir
+cp $tmp_dir/.vimrc ~/
+rm -rf $tmp_dir
+
+echo "installing & updating Vundle..."
+if test ! -e "~/.vim/bundle/Vundle.vim"; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ".vim/bundle/Vundle.vim"
+fi
+
+vim +BundleInstall +qall
+
+cd $old_dir
